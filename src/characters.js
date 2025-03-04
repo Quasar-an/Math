e1464b8b10c076ce91faf96777f3751219638e74
Angelina <pankova1797@gmail.com>
2025-03-04 21:07:24 +0300

    edits

--- src/characters.js
+++ src/characters.js
@@ -1,52 +1,63 @@
 class Character {
-    constructor(name, type) {
+  constructor(name, type) {
       if (!name || typeof name !== 'string' || name.length < 2 || name.length > 10) {
-        throw new Error('Имя должно быть строкой длиной от 2 до 10 символов');
+          throw new Error('Имя должно быть строкой длиной от 2 до 10 символов');
       }
       if (!['Magician', 'Daemon'].includes(type)) {
-        throw new Error('Неверный тип персонажа');
+          throw new Error('Неверный тип персонажа');
       }
-  
+
       this.name = name;
       this.type = type;
-      this.attack = 100;
+      this._attack = 100;
       this._stoned = false;
-    }
+  }
 
-    attackPower(distance) {
-        if (distance < 1 || distance > 5) {
-          throw new Error('Расстояние должно быть от 1 до 5 клеток');
-        }
-      
-        let baseAttack = this.attack * (1 - 0.1 * (distance - 1));
-        if (this._stoned) {
-          baseAttack -= Math.log2(distance) * 5;
-        }
-        return Math.max(baseAttack, 0);
+  get attack() {
+      return this._attack;
+  }
+
+  set attack(value) {
+      if (typeof value !== 'number' || value < 0) {
+          throw new Error('Атака должна быть числом больше или равным 0');
       }
- 
-    set stoned(value) {
+      this._attack = value;
+  }
+
+  attackPower(distance) {
+      if (distance < 1 || distance > 5) {
+          throw new Error('Расстояние должно быть от 1 до 5 клеток');
+      }
+
+      let baseAttack = this.attack * (1 - 0.1 * (distance - 1));
+      if (this._stoned) {
+          baseAttack -= Math.log2(distance) * 5;
+      }
+      return Math.max(baseAttack, 0);
+  }
+
+  set stoned(value) {
       if (typeof value !== 'boolean') {
-        throw new Error('Состояние "дурмана" должно быть boolean');
+          throw new Error('Состояние "дурмана" должно быть boolean');
       }
       this._stoned = value;
-    }
-  
-    get stoned() {
-      return this._stoned;
-    }
   }
 
-  class Magician extends Character {
-    constructor(name) {
+  get stoned() {
+      return this._stoned;
+  }
+}
+
+class Magician extends Character {
+  constructor(name) {
       super(name, 'Magician');
-    }
   }
-  
-  class Daemon extends Character {
-    constructor(name) {
+}
+
+class Daemon extends Character {
+  constructor(name) {
       super(name, 'Daemon');
-    }
   }
-  
-  module.exports = { Magician, Daemon, Character };
+}
+
+module.exports = { Magician, Daemon, Character };
