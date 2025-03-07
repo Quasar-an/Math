a9d30876de9f55794fcc588b5b6a51d2bfaae68a
Angelina <pankova1797@gmail.com>
2025-03-07 17:21:44 +0300

    new edits

--- src/characters.js
+++ src/characters.js
@@ -1,63 +1,75 @@
 class Character {
-  constructor(name, type) {
+    constructor(name, type) {
       if (!name || typeof name !== 'string' || name.length < 2 || name.length > 10) {
-          throw new Error('Имя должно быть строкой длиной от 2 до 10 символов');
+        throw new Error('Имя должно быть строкой длиной от 2 до 10 символов');
       }
       if (!['Magician', 'Daemon'].includes(type)) {
-          throw new Error('Неверный тип персонажа');
+        throw new Error('Неверный тип персонажа');
       }
-
+  
       this.name = name;
       this.type = type;
+      this._distance = 1;
       this._attack = 100;
       this._stoned = false;
-  }
-
-  get attack() {
-      return this._attack;
-  }
-
-  set attack(value) {
+    }
+  
+    get attack() {
+      return this.attackPower(this._distance);
+    }
+  
+    set attack(value) {
       if (typeof value !== 'number' || value < 0) {
-          throw new Error('Атака должна быть числом больше или равным 0');
+        throw new Error('Атака должна быть числом больше или равным 0');
       }
       this._attack = value;
-  }
-
-  attackPower(distance) {
-      if (distance < 1 || distance > 5) {
-          throw new Error('Расстояние должно быть от 1 до 5 клеток');
+    }
+  
+    set distance(value) {
+      if (value < 1 || value > 5) {
+        throw new Error('Расстояние должно быть от 1 до 5');
       }
-
-      let baseAttack = this.attack * (1 - 0.1 * (distance - 1));
+      this._distance = value;
+    }
+  
+    get distance() {
+      return this._distance;
+    }
+  
+    attackPower(distance) {
+      if (distance < 1 || distance > 5) {
+        throw new Error('Расстояние должно быть от 1 до 5 клеток');
+      }
+  
+      let baseAttack = this._attack * (1 - 0.1 * (distance - 1));
       if (this._stoned) {
-          baseAttack -= Math.log2(distance) * 5;
+        baseAttack -= Math.log2(distance) * 5;
       }
       return Math.max(baseAttack, 0);
-  }
+    }
 
-  set stoned(value) {
+    set stoned(value) {
       if (typeof value !== 'boolean') {
-          throw new Error('Состояние "дурмана" должно быть boolean');
+        throw new Error('Состояние "дурмана" должно быть boolean');
       }
       this._stoned = value;
-  }
-
-  get stoned() {
+    }
+  
+    get stoned() {
       return this._stoned;
+    }
   }
-}
-
-class Magician extends Character {
-  constructor(name) {
+  
+  class Magician extends Character {
+    constructor(name) {
       super(name, 'Magician');
+    }
   }
-}
-
-class Daemon extends Character {
-  constructor(name) {
+  
+  class Daemon extends Character {
+    constructor(name) {
       super(name, 'Daemon');
+    }
   }
-}
-
-module.exports = { Magician, Daemon, Character };
+  
+  module.exports = { Magician, Daemon, Character };
